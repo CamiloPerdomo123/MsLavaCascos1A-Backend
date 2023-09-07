43fc36acd4ee9f1877338a94d8c48848aa280839
Juan Camilo Perdomo Carreño <perdomo120120@hotmail.com>
2023-09-06 21:50:26 -0500

    feat: Se crear el Ms

---
+++ src/main/java/com/lavacascos/MslavacascosApplication.java
@@ -0,0 +1,13 @@
+package com.lavacascos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MslavacascosApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MslavacascosApplication.class, args);
+    }
+
+}
